60c36f82c655155760ada6f9770f5b61cb201b2c
Josh Thompson <thompsonjoshd@gmail.com>
2021-03-15 09:22:14 -0600

    passes dragon_test.rb

---
+++ mythical-creatures/lib/dragon.rb
@@ -0,0 +1,17 @@
+class Dragon
+  attr_reader :name, :color, :rider, :meal_count
+  def initialize(name, color, rider)
+    @name = name
+    @color = color
+    @rider = rider
+    @meal_count = 0
+  end
+  
+  def hungry?
+    meal_count < 3
+  end
+  
+  def eat
+    @meal_count += 1
+  end
+end
